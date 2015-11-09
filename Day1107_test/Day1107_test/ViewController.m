214f46a42c382e3c6207f9956067ff85e06fa502
eru71 <colette71@foxmail.com>
2015-11-09 12:26:37 +0800

    自动布局代码布置的第三方库

--- Day1107_test/Day1107_test/ViewController.m
+++ Day1107_test/Day1107_test/ViewController.m
@@ -8,8 +8,11 @@
 
 #import "ViewController.h"
 #import <BaiduMapAPI_Map/BMKMapComponent.h>
-@interface ViewController ()<BMKMapViewDelegate>
+#import <CoreLocation/CoreLocation.h>
+@interface ViewController ()<BMKMapViewDelegate,BMKLocationServiceDelegate>
 @property (nonatomic,strong) BMKMapView *mapView;
+@property (nonatomic,strong) CLLocationManager *manager;
+@property (nonatomic,strong) BMKLocationService *locService;
 @end
 
 @implementation ViewController
@@ -19,7 +22,30 @@
     // Do any additional setup after loading the view, typically from a nib.
     _mapView = [[BMKMapView alloc]initWithFrame:CGRectMake(0, 0, 320, 480)];
     self.view = _mapView;
+    //初始化BMKLocationService
+    _locService = [[BMKLocationService alloc]init];
+    _locService.delegate = self;
+    //启动LocationService
+    [_locService startUserLocationService];
+}
+//实现相关delegate 处理位置信息更新
+//处理方向变更信息
+- (void)didUpdateUserHeading:(BMKUserLocation *)userLocation
+{
+    //NSLog(@"heading is %@",userLocation.heading);
+}
+//处理位置坐标更新
+- (void)didUpdateBMKUserLocation:(BMKUserLocation *)userLocation
+{
+    //NSLog(@"didUpdateUserLocation lat %f,long %f",userLocation.location.coordinate.latitude,userLocation.location.coordinate.longitude);
+    //普通态
+    //以下_mapView为BMKMapView对象
+    _mapView.showsUserLocation = YES;//显示定位图层
+    [_mapView updateLocationData:userLocation];
     
+    BMKCoordinateSpan span = BMKCoordinateSpanMake(0.01, 0.01);
+    BMKCoordinateRegion region = BMKCoordinateRegionMake(userLocation.location.coordinate, span);
+    [self.mapView setRegion:region animated:YES];
 }
 
 -(void)viewWillAppear:(BOOL)animated
@@ -36,6 +62,16 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(CLLocationManager *)manager{
+    if (!_manager) {
+        _manager = [[CLLocationManager alloc]init];
+        if ([UIDevice currentDevice].systemVersion.floatValue>=8.0) {
+            [_manager requestAlwaysAuthorization];
+        }
+    }
+    return _manager;
 }
 
 @end
